f87bd88baa4eea0a4772498077732eeeb8d3596f
zhang6018 <k20gjk04@p.is.kyusan-u.ac.jp>
2020-05-25 10:28:19 +0900

    ruby入門

---
+++ if.rb
@@ -0,0 +1,10 @@
+puts "number?"
+a=gets.to_i
+puts a
+puts a*2
+
+if a%2 == 0
+    puts "even"
+else
+    puts "odd"
+end
---
+++ var.rb
@@ -0,0 +1,18 @@
+s="Hello"
+t="Ruby"
+puts s
+puts s+t
+
+s="Ruby"
+b=3
+puts s
+puts s*b
+
+puts 5+3
+puts "5"+"3"
+
+puts 5*3
+puts "5"*3
+
+puts 5-3
+puts "5"-3
