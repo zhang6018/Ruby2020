d016c3384270e87145244f3edb28b59623290407
zhang6018 <k20gjk04@p.is.kyusan-u.ac.jp>
2020-06-01 09:36:12 +0900

    配列の練習

---
+++ other/Array.java
@@ -0,0 +1,18 @@
+ia=[10,20,30,40]
+
+i=0
+while i<ia.length
+    print ia[i]
+    print ","
+    i+=1
+end
+puts
+
+sa=["January","February","March"]
+i=0
+while i<sa.length
+    print sa[i]
+    print ","
+    i+=1
+end
+puts
