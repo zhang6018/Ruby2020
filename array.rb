95dd7d8388cdf0210dfac8ff332cb098922fc5d6
zhang6018 <k20gjk04@p.is.kyusan-u.ac.jp>
2020-06-01 10:41:32 +0900

    配列の練習

--- array.rb
+++ array.rb
@@ -1,4 +1,4 @@
-ia=[10,20,30,40]
+ia=[10,20,30,40,"January","February","March",3.14,0.28,[1,2,"a",5]]
 
 i=0
 while i<ia.length
@@ -14,5 +14,11 @@ while i<sa.length
     print sa[i]
     print ","
     i+=1
+
 end
 puts
+
+da=[[1,2,3],[3,5,6][7,8,9]]
+p da
+p da[1]
+p da[2][0]
---
+++ array2.rb
@@ -0,0 +1,11 @@
+ia=[1,3,5,"Jan",7]
+
+ia.each do |i|
+    print i.to_s + ","
+end
+puts
+
+[1,3,5,"Jan",7].each do |i|
+    print i.to_s + ","
+end
+puts
